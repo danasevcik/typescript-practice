150aed64ffab670b46643de762e0530df50f344d
Dana Sevcik <danasevcik@hotmail.com>
2019-10-26 14:53:24 -0400

    interface typescript

--- greeter.ts
+++ greeter.ts
@@ -3,10 +3,26 @@
 // adds type to function argument
 // changes var to let
 
-function greeter(person: string) {
-    return "Hello, " + person;
+// function greeter(person: string) {
+//     return "Hello, " + person;
+// }
+//
+// let user = "Jane User";
+//
+// document.body.textContent = greeter(user);
+
+// interface syntax
+// interface describes an object
+// different from class in typescript - doesn't provide implementation or initialization
+interface Person {
+    firstName: string;
+    lastName: string;
 }
 
-let user = "Jane User";
+function greeter(person: Person) {
+    return "Hello, " + person.firstName + " " + person.lastName;
+}
+
+let user = { firstName: "Jane", lastName: "User" };
 
 document.body.textContent = greeter(user);
