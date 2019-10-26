cedd19162d40253aae579a563a8ca12a0847fb2d
Dana Sevcik <danasevcik@hotmail.com>
2019-10-26 14:55:47 -0400

    class and public keywords typescript

--- greeter.ts
+++ greeter.ts
@@ -14,6 +14,31 @@
 // interface syntax
 // interface describes an object
 // different from class in typescript - doesn't provide implementation or initialization
+
+// interface Person {
+//     firstName: string;
+//     lastName: string;
+// }
+//
+// function greeter(person: Person) {
+//     return "Hello, " + person.firstName + " " + person.lastName;
+// }
+//
+// let user = { firstName: "Jane", lastName: "User" };
+//
+// document.body.textContent = greeter(user);
+
+// class syntax
+// provides more functionality than interface - can use constructor functionality
+// OO design
+// using public keyword in constructor allows us to create properties with that name
+class Student {
+    fullName: string;
+    constructor(public firstName: string, public middleInitial: string, public lastName: string) {
+        this.fullName = firstName + " " + middleInitial + " " + lastName;
+    }
+}
+
 interface Person {
     firstName: string;
     lastName: string;
@@ -23,6 +48,6 @@ function greeter(person: Person) {
     return "Hello, " + person.firstName + " " + person.lastName;
 }
 
-let user = { firstName: "Jane", lastName: "User" };
+let user = new Student("Jane", "M.", "User");
 
 document.body.textContent = greeter(user);
